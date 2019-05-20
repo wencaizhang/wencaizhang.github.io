008340d9ce19bff75936995185284b1a82d7d8b7
wencaizhang <1052642137@qq.com>
2019-05-20 22:55:01 +0800

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,7 @@ const microCode = require('./sidebar/micro-code');
 
 module.exports = {
   base: '/',
-  title: '前端杂货铺',
+  title: '前端锦囊',
   description: '',
   head: [
     ['script', {
@@ -31,24 +31,7 @@ module.exports = {
   themeConfig: {
     // 百度统计
     baidu_tongji: "969e7a19e58c941d5db2a9bf87885340",
-    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-    repo: 'wencaizhang/oh-my-js',
-    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-    repoLabel: 'GitHub',
 
-    // 以下为可选的编辑链接选项
-
-    // 假如你的文档仓库和项目本身不在一个仓库：
-    docsRepo: 'wencaizhang/oh-my-js',
-    // 假如文档不是放在仓库的根目录下：
-    docsDir: 'docs',
-    // 假如文档放在一个特定的分支下：
-    docsBranch: 'master',
-    // 默认是 false, 设置为 true 来启用
-    editLinks: true,
-    // 默认为 "Edit this page"
-    editLinkText: '帮助我们改善此页面！',
     nav: [{
         text: '主页',
         link: '/'
@@ -91,7 +74,6 @@ module.exports = {
       '/underscore-analysis/': undercore,
       '/micro-code/': microCode,
     }
-
 
   },
 }
