80a15fa8029951b574664683946825c21c587557
wencaizhang <1052642137@qq.com>
2019-05-22 16:01:44 +0800

    create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+# 进入生成的文件夹
+cp ./README.md src/.vuepress/dist
+cd src/.vuepress/dist
+
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+currTime=`date +%Y-%m-%d-%H-%M-$S`
+
+git init
+git add -A
+git commit -m ":rocket: deploy ${currTime}"
+
+git push -f https://github.com/wencaizhang/wencaizhang.github.io.git master
+
+cd -
