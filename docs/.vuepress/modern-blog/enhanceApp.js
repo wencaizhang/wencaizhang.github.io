4d4a0b75e2d446dc9fed586b77af2da183c3bf27
wencaizhang <1052642137@qq.com>
2020-07-06 14:53:54 +0800

    elementui 按需引入

--- docs/.vuepress/modern-blog/enhanceApp.js
+++ docs/.vuepress/modern-blog/enhanceApp.js
@@ -1,4 +1,19 @@
-import ElementUI from 'element-ui'
+import {
+  Autocomplete,
+  Button,
+  Card,
+  Menu,
+  Container,
+  Badge,
+  Row,
+  Col,
+  Link,
+  Icon,
+  Main,
+  MenuItem,
+  Carousel,
+  CarouselItem,
+} from 'element-ui'
 import Translation from '@theme/plugins/Translation'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -9,6 +24,29 @@ export default ({
   router, // the router instance for the app
   siteData, // site metadata
 }) => {
-  Vue.use(ElementUI)
+  // global helper for adding base path to absolute urls
+  Vue.prototype.$withBase = function (path) {
+    if (!path) return ''
+    const base = this.$site.base
+    if (path.charAt(0) === '/') {
+      return base + path.slice(1)
+    } else {
+      return path
+    }
+  }
+  Vue.use(Autocomplete)
+  Vue.use(Button)
+  Vue.use(Card)
+  Vue.use(Menu)
+  Vue.use(Container)
+  Vue.use(Badge)
+  Vue.use(Row)
+  Vue.use(Col)
+  Vue.use(Link)
+  Vue.use(Icon)
+  Vue.use(Main)
+  Vue.use(MenuItem)
+  Vue.use(Carousel)
+  Vue.use(CarouselItem)
   Vue.use(Translation)
 }
