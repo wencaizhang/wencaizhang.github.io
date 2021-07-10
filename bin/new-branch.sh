be2727c9b9a6d7255c82bcfdce47ac1a69f66c86
wencaizhang <1052642137@qq.com>
2021-07-10 22:44:53 +0800

    new site powered by hugo with theme meme

---
+++ bin/new-branch.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 定义变量
+destDir="public"
+currTime1=`date +%Y-%m-%d`
+currTime2=`date +%H:%M:%S`
+commit=":rocket: deploy @${currTime1} ${currTime2}"
+repo="https://github.com/wencaizhang/wencaizhang.github.io.git"
+remoteBranch="master"
+domain="coolfe.fun"
+
+cp -r .github $destDir
+
+# 进入生成的文件夹
+cd $destDir
+
+
+# 发布到自定义域名
+echo $domain > CNAME
+
+
+# 将前端静态文件初始化为 git 仓库并提交
+git init
+git add -A
+git commit -m "$commit"
+
+git push -f $repo master:$remoteBranch
+
+# 回到上次工作目录
+cd -
