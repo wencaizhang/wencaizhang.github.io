84dc3eda81860e165c775fe63cd3fae71cc473e8
hta218 <huynhtuananh21895@gmail.com>
2023-09-04 17:24:03 +0700

    style: update snippet card

--- components/SnippetCard.tsx
+++ components/SnippetCard.tsx
@@ -11,11 +11,11 @@ export function SnippetCard({ snippet }: { snippet: SnippetFrontMatter }) {
         data-umami-event="view-snippet"
         className="mb-4 flex cursor-pointer rounded-lg border border-slate-200 shadow-intense hover:shadow-nextjs dark:border-gray-600 dark:hover:border-gray-400 lg:mb-0"
       >
-        <div className="p-3 lg:p-4">
+        <div className="flex items-center justify-center">
           <BrandIcon type={type} />
         </div>
-        <div className="overflow-hidden p-3 md:p-4 lg:p-4 space-y-2">
-          <h3 className="overflow-hidden overflow-ellipsis whitespace-nowrap text-lg font-bold leading-8 tracking-tight lg:text-2xl">
+        <div className="overflow-hidden space-y-2">
+          <h3 className="overflow-hidden overflow-ellipsis whitespace-nowrap text-lg font-bold leading-8 tracking-tight lg:text-xl">
             {heading}
           </h3>
           <p className="text-gray-700 dark:text-gray-400 lg:text-base elip-line-2">{summary}</p>
