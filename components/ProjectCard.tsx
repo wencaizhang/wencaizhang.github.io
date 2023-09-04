8d4a0323eefc3ffac8fbd2b9d71ad493e7938633
wencaizhang <1052642137@qq.com>
2023-09-04 23:22:06 +0800

    feat: update projects card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'next-i18next'
+import { Github } from 'lucide-react'
 import useSWR from 'swr'
 import type { ProjectCardProps } from '~/types/components'
 import type { GithubRepository } from '~/types/server'
@@ -53,8 +54,10 @@ export function ProjectCard({ project }: ProjectCardProps) {
   let { t } = useTranslation('common')
   let { title, description, imgSrc, url, repo, builtWith, state } = project
   let { data } = useSWR(`/api/github?repo=${repo}`, fetcher)
-  let repository: GithubRepository = data?.repository
-  let href = repository?.url || url
+  // let repository: GithubRepository = data?.repository
+  // let href = repository?.url || url
+  let repository = repo
+  let href = repo
 
   return (
     <div className="md p-4 md:w-1/2" style={{ maxWidth: '544px' }}>
@@ -70,17 +73,34 @@ export function ProjectCard({ project }: ProjectCardProps) {
         {getState(state)}
         <div className="flex grow flex-col justify-between space-y-6 p-4 md:p-6">
           <div className="space-y-3">
-            <h2 className="text-2xl font-bold leading-8 tracking-tight">
-              {href ? (
-                <Link href={href} aria-label={`Link to ${title}`}>
-                  <span data-umami-event="project-title-link">{title}</span>
-                </Link>
-              ) : (
-                title
+            <div className="flex justify-between">
+              <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                {href ? (
+                  <Link href={href} aria-label={`Link to ${title}`}>
+                    <span data-umami-event="project-title-link">{title}</span>
+                  </Link>
+                ) : (
+                  title
+                )}
+              </h2>
+
+              {href && (
+                <a
+                  href={href}
+                  title={t('projects.source_code')}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="flex items-center space-x-1"
+                  data-umami-event="project-repo"
+                >
+                  <Github size={20} strokeWidth={2} />
+                </a>
               )}
-            </h2>
+            </div>
+
             <div className="max-w-none space-y-2 text-gray-500 dark:text-gray-400">
-              <p>{repository?.description || description}</p>
+              {/* <p>{repository?.description || description}</p> */}
+              <p>{description}</p>
               <div className="flex flex-wrap gap-2">
                 {/* <span className="shrink-0">{t('projects.built_with')}:</span> */}
                 {builtWith?.map((tool, index) => {
@@ -98,17 +118,17 @@ export function ProjectCard({ project }: ProjectCardProps) {
               </div>
             </div>
           </div>
-          {repository ? (
+          {/* {repository ? (
             <GithubRepo repo={repository} />
-          ) : (
-            <Link
-              href={url}
-              className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-              aria-label={`Link to ${title}`}
-            >
-              <span data-umami-event="project-learn-more">{t('projects.learn_more')} &rarr;</span>
-            </Link>
-          )}
+          ) : ( */}
+          <Link
+            href={url}
+            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label={`Link to ${title}`}
+          >
+            <span data-umami-event="project-learn-more">{t('projects.learn_more')} &rarr;</span>
+          </Link>
+          {/* )} */}
         </div>
       </div>
     </div>
